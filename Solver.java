04f67508fbb26b4a2cd976a292d8d631ca8ef03a
tarasgovda <taras.govda@gmail.com>
2018-12-24 19:24:39 +0200

    added blueprint of solver class

---
+++ Solver.java
@@ -0,0 +1,27 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Solver {
+
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvable() {
+
+    }
+    public int moves() {
+
+    }
+
+    public Iterable<Board> solution() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
