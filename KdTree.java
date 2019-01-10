0fd4afba72e50de45e6b7c02902842df8187503d
tarasgovda <taras.govda@gmail.com>
2019-01-10 18:23:20 +0200

    colinear points, fix

--- KdTree.java
+++ KdTree.java
@@ -4,7 +4,34 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
 public class KdTree {
+
+
+    private static class Node {
+        private Point2D p;
+        private RectHV rect;
+        private Node lb;
+        private Node rt;
+    }
+
+
+    private Node root;
+    private int size;
+
+    public KdTree() {
+
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
 
 
 
@@ -12,4 +39,5 @@ public class KdTree {
     public static void main(String[] args) {
 
     }
+
 }
